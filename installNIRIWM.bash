661267c82e3f1e275d17e5ccdbfd44822e519a92
matte <duskbladez83@gmail.com>
2026-06-03 19:24:57 +0100

    Refactor installation commands in installNIRIWM.bash
    Combined multiple commands into a single line for installation and added a completion message.

--- installNIRIWM.bash
+++ installNIRIWM.bash
@@ -1,9 +1,2 @@
 # For installing NIRI WM external (others are still normal)
-sudo
-cd .config
-
-git clone https://github.com/dsksnkz/my-hyprland-rice.git
-
-pacman -S niri-git cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix 
-
-exit
+sudo cd .config && git clone https://github.com/dsksnkz/my-hyprland-rice.git && pacman -S niri-git cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix && echo Installation either completed or failed. You may now close this Window or report a bug. 
